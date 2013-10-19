f1477093fec73c034f1b0c67a4a9e37ff513fd0e
James A. Rosen <james@zendesk.com>
2013-10-19 09:44:44 -0700

    AhabApplication: allow reloading in development

--- lib/ahab_application.rb
+++ lib/ahab_application.rb
@@ -1,9 +1,14 @@
 require 'sinatra'
+require "sinatra/reloader" if development?
 
 class AhabApplication < Sinatra::Base
   set(:project_root)  { File.expand_path('../../', __FILE__) }
   set(:public_folder) { File.join(project_root, 'public') }
   set(:css_dir)       { public_folder }
+
+  configure :development do
+    register Sinatra::Reloader
+  end
 
   get '/' do
     erb :index
