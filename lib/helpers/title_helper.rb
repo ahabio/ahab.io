eb3e5eacb0ab9bf6dc41e546767c6f53f28409b1
James A. Rosen <james@zendesk.com>
2013-10-20 16:39:29 -0700

    put Moby in the header

--- lib/helpers/title_helper.rb
+++ lib/helpers/title_helper.rb
@@ -1,5 +1,7 @@
 module TitleHelper
 
+  include AssetHelpers
+
   attr_accessor :page_title
 
   def head_title
@@ -10,7 +12,7 @@ module TitleHelper
 
   def site_title
     if show_header_title?
-      link = "<a href='/'>ahab.io</a>"
+      link = "<a href='/'>#{img 'moby2x.png'}</a>"
       "<h1 class='grid_2'>#{link}</h1>"
     else
       "<div class='grid_2'>&nbsp;</div>"
