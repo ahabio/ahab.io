68f248e36f1da9608cf3bbaedc3564dfeec4f2d1
James A. Rosen <james@zendesk.com>
2013-10-20 16:00:05 -0700

    index: list some hand-picked favories for "popular"

--- lib/helpers/index_helper.rb
+++ lib/helpers/index_helper.rb
@@ -2,4 +2,14 @@ module IndexHelper
   def total_assets
     Asset.total_with_format
   end
+
+  def popular_assets
+    Asset.where(name: %w{jquery 960gs handlebars base64 ember lodash.js zepto}).
+      map do |asset|
+        v = asset.optimistic_version
+        AssetVersion.find_by asset_id: asset.id, value: v
+      end.
+      compact.
+      slice(0, 5)
+  end
 end
