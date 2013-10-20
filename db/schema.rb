420ed67cd80194ba1e5d7dd3a25283dd7e3ff6dd
James A. Rosen <james@zendesk.com>
2013-10-20 12:22:18 -0700

    assets.description is a text column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131020190641) do
+ActiveRecord::Schema.define(version: 20131020192100) do
 
   create_table "asset_versions", force: true do |t|
     t.string  "value",    null: false
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 20131020190641) do
   create_table "assets", force: true do |t|
     t.string "name",        null: false
     t.string "homepage"
-    t.string "description"
+    t.text   "description"
   end
 
 end
