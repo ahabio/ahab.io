54cdd0977dd8c338fb2837619cc5910ced31d7e3
James A. Rosen <james@zendesk.com>
2013-10-19 23:03:44 -0700

    Add AssetHelpers
    Adds `js`, `css`, `img`, `js_url`, `css_url`, and `image_url` helper
    methods to templates.

---
+++ lib/helpers/asset_helpers.rb
@@ -0,0 +1,29 @@
+module AssetHelpers
+
+  def js(name)
+    "<script src='#{js_url(name)}' type='text/javascript'></script>"
+  end
+
+  def css(name)
+    "<link href='#{css_url(name)}' rel='stylesheet' type='text/css'>"
+  end
+
+  def img(name)
+    "<img src='#{image_url(name)}' />"
+  end
+
+  def js_url(name)
+    asset_url "#{name}.js"
+  end
+
+  def css_url(name)
+    asset_url "#{name}.css"
+  end
+
+  def asset_url(name)
+    "/#{name}"
+  end
+
+  alias_method :image_url, :asset_url
+
+end
