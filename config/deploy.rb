08a3da528eefc6e23e6fcf6e878f4b9fb217e21f
James A. Rosen <james@zendesk.com>
2013-10-20 12:51:07 -0700

    deploy: symlink database.yml

--- config/deploy.rb
+++ config/deploy.rb
@@ -75,6 +75,10 @@ after 'deploy:update_code', :roles => :app do
   run "cd #{latest_release} && bin/rake assets:compile assets:purge"
 end
 
+task :symlink, :except => { :no_release => true } do
+  run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+end
+
 # Restart Passenger
 deploy.task :restart, :roles => :app do
   # Fix Permissions
